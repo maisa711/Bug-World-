f57e4871fd8050d960d9b6c18d1ff98f4c1188b4
maisa711 <43515290+maisa711@users.noreply.github.com>
2023-03-19 00:17:08 +0100

    added comments

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,49 +1,52 @@
+// gameStats is an array of objects that contains the title, id and value of game statistic
 let gameStats = [
     {
-      title: "Iterations",
-      id: "iteration-count",
-      value: "200/500",
+        title: "Iterations",
+        id: "iteration-count",
+        value: "200/500",
     },
     {
-      title: "Amount of undetected food",
-      id: "undetected-food",
-      value: "20",
+        title: "Amount of undetected food",
+        id: "undetected-food",
+        value: "20",
     },
     {
-      title: "Red bugs Remaining",
-      id: "red-bugs-left",
-      value: "5",
+        title: "Red bugs Remaining",
+        id: "red-bugs-left",
+        value: "5",
     },
     {
-      title: "Red bugs Killed",
-      id: "red-bugs-killed",
-      value: "19",
+        title: "Red bugs Killed",
+        id: "red-bugs-killed",
+        value: "19",
     },
     {
-      title: "Food Brought home by red bugs",
-      id: "red-brought-food",
-      value: "4",
+        title: "Food Brought home by red bugs",
+        id: "red-brought-food",
+        value: "4",
     },
     {
-      title: "Black bugs Remaining",
-      id: "black-bugs-left",
-      value: "7",
+        title: "Black bugs Remaining",
+        id: "black-bugs-left",
+        value: "7",
     },
     {
-      title: "Black bugs Killed",
-      id: "black-bugs-killed",
-      value: "8",
+        title: "Black bugs Killed",
+        id: "black-bugs-killed",
+        value: "8",
     },
     {
-      title: "Food Brought home by black bugs",
-      id: "black-brought-food",
-      value: "9",
+        title: "Food Brought home by black bugs",
+        id: "black-brought-food",
+        value: "9",
     },
-  ];
+];
 
-  let gStat = document.getElementById("game-stats");
+// get the game stats div
+let gStat = document.getElementById("game-stats");
 
-  gameStats.forEach((stat) => {
+// create a div including the title, id and value for each game statistic and append it to the game stats div
+gameStats.forEach((stat) => {
     const newDiv = document.createElement("div");
     const newStrong = document.createElement("strong");
     const newSpan = document.createElement("span");
@@ -53,24 +56,25 @@ let gameStats = [
     newDiv.appendChild(newStrong);
     newDiv.appendChild(newSpan);
     gStat.appendChild(newDiv);
-  });
+});
 
-  function popup(btn) {
-    
+// function to open the module pop-up
+function popup(btn) {
+
+    // get the overlay and close button
     const overlay = document.getElementById(btn.id + "-popup");
     const closeBtn = overlay.querySelector("#close-btn");
 
     // Function to open the module pop-up
     function openModule() {
-      overlay.style.display = "flex";
+        overlay.style.display = "flex";
     }
 
     // Function to close the module pop-up
     function closeModule() {
-      overlay.style.display = "none";
+        overlay.style.display = "none";
     }
-    
-   
+
     closeBtn.addEventListener("click", closeModule);
     openModule();
-  }
+}
--- js/uploadPage.js
+++ js/uploadPage.js
@@ -1,24 +1,37 @@
+// Description: This file contains the functions that are used to upload the files to the local storage
 function SaveFile(inp) {
     const file = inp.files[0];
 
+    // create a new FileReader object
     const reader = new FileReader();
+
+    // add an event listener to deal with the file when the reader is done
     reader.addEventListener(
-      "load",
-      () => {
-        localStorage.setItem(inp.id, reader.result); // save contents to localStorage
-      },
-      false
+        "load",
+        () => {
+            // file validation goes here
+            
+            //save file to localStorage, key is the id of the input element
+            localStorage.setItem(inp.id, reader.result);
+        },
+        false
     );
 
+    // reads the file as text
     if (file) {
-      reader.readAsText(file);
+        reader.readAsText(file);
     }
-  }
+}
 
-  function saveIterations(inp) {
-    localStorage.setItem(inp.id, inp.value); // save contents to localStorage
-  }
+// save number of iterations to localStorage, key is the id of the input element
+function saveIterations(inp) {
+    if (inp.value > 0 && !inp.value == "") {
+        localStorage.setItem(inp.id, inp.value);
+    }
 
-  function saveLog(inp) {
-    localStorage.setItem(inp.id, inp.checked); // save contents to localStorage
-  }
+}
+
+// save log info to localStorage, key is the id of the input element
+function saveLog(inp) {
+    localStorage.setItem(inp.id, inp.checked);
+}
